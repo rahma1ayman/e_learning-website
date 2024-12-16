255fb8ed0a7c18898c5104316e580404f335c5e3
Rahma Ayman <rahma1ayman@gmail.com>
2024-12-16 21:15:31 +0200

    forget_password

--- e_learning-website-main/registration/login.php
+++ e_learning-website-main/registration/login.php
@@ -1,26 +1,25 @@
 <?php
 session_start();
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-  
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
   $email = strtolower($_POST['email']);
   $password = $_POST['password'];
 
   $conn = mysqli_connect("localhost", "root", "", "e_learning");
   if (!$conn)
-      echo mysqli_connect_error();
+    echo mysqli_connect_error();
 
   $sql = "SELECT s_email, s_password, s_name FROM student WHERE s_email = '$email' AND s_password = '$password'";
   $login = mysqli_query($conn, $sql);
 
-  if(mysqli_num_rows($login) == 0){                             //counts the number of rows
-      $error_message = "Invalid email or password.";            //if number of rows returned is 0, creates an error message
-  } else 
-  {
-      $_SESSION['is_logged_in'] = true;                         //if query matches data, moves to home page and changes
-      $row = mysqli_fetch_assoc($login);                        //fetches database query data
-      $_SESSION['username'] = $row['s_name'];                   //extracts s_name to be used in welcome string
-      header('location: ../home/home.php');                          //log in and sign up buttons to a log out button and a welcome string
-      exit();
+  if (mysqli_num_rows($login) == 0) {                             //counts the number of rows
+    $error_message = "Invalid email or password.";            //if number of rows returned is 0, creates an error message
+  } else {
+    $_SESSION['is_logged_in'] = true;                         //if query matches data, moves to home page and changes
+    $row = mysqli_fetch_assoc($login);                        //fetches database query data
+    $_SESSION['username'] = $row['s_name'];                   //extracts s_name to be used in welcome string
+    header('location: ../home/home.php');                          //log in and sign up buttons to a log out button and a welcome string
+    exit();
   }
 }
 ?>
@@ -32,7 +31,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+  <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="../home/styles.css">
 
@@ -97,16 +96,62 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
       </div>
       <button type="submit">Log in</button>
       <br>
-      
-      <h6 style="text-align: center;">Forget password? <a href="forget_password.html">Forget Password</a></h6>
-
+      <h6 style="padding-top:10px;text-align: center;">Forget password?
+        <button type="button" class="btn btn-link p-0 reset-pass" style="text-decoration: underline; font-size: inherit; margin-left: 0;" data-toggle="modal" data-target="#forgetModal">
+          Reset Password
+        </button>
+      </h6>
       <div class="divider">or login with</div>
 
       <h6 style="text-align: center;">
         Don't have an account? <a href="signup.php">Sign up</a> </h6>
     </form>
   </div>
+
+  <!-- forget Modal -->
+  <div class="modal fade" id="forgetModal" tabindex="-1" role="dialog" aria-labelledby="forgetModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header" style="justify-content: space-between;">
+          <h5 class="modal-title" id="forgetModalLabel">Reset Password</h5>
+          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"
+            style="background: none; border: none; padding: 0; margin: 0; font-size: 1.5rem; line-height: 1;">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+
+          <div class="mb-3">
+            <label for="email" class="form-label" style="font-size: 22px;">Email</label>
+            <input type="email" class="form-control" id="email" placeholder="Email">
+          </div>
+
+          <div class="mb-3">
+            <label for="password" class="form-label" style="font-size: 22px;">New Password</label>
+            <input type="text" class="form-control" id="password" placeholder="new password">
+          </div>
+
+          <div class="mb-3">
+            <label for="cnf_password" class="form-label" style="font-size: 22px;">Confirm Password</label>
+            <input type="email" class="form-control" id="cnf_password" placeholder="confirm password">
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-primary"
+            style="background-color: #C3413B; color: #fff; border-radius: 5px; border: none; 
+         padding: 10px 20px; font-size: 1rem; font-weight: 600; cursor: pointer; 
+         transition: background-color 0.3s ease;"
+            onmouseover="this.style.backgroundColor='#A82E29';"
+            onmouseout="this.style.backgroundColor='#C3413B';">
+            Reset Password</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
